7b91eaa019f02de8a2e2537bbf2a92cb793ee5f2
Alex Messias <claudioalex16@gmail.com>
2022-05-02 16:18:44 -0300

    atualiznado'

--- database/migrations/2022_05_02_153738_academicos.php
+++ database/migrations/2022_05_02_153738_academicos.php
@@ -16,6 +16,7 @@ class Academicos extends Migration
         Schema::create('academicos', function (Blueprint $table) {
             $table->id();
             $table->string('ra');
+
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->rememberToken();
